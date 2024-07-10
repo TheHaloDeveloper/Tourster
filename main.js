7adae3815a4562f81f1364ad87534dd95e17ba07
Halo <anye.dev.meltdown@gmail.com>
2024-07-09 21:27:35 -0700

    Add messages to screen from input

--- main.js
+++ main.js
@@ -1,8 +1,17 @@
 let message_input = document.getElementById("message_input")
+let messages_list = document.getElementById("messages_list")
 
 function message_send() {
-    console.log(message_input.value)
+    let msg = document.createElement('li')
+    msg.classList += "list-user"
+    msg.innerHTML = message_input.value
+    messages_list.appendChild(msg)
     message_input.value = ''
+
+    let ai_msg = document.createElement('li')
+    ai_msg.classList += "list-ai"
+    ai_msg.innerHTML = "Message Received!"
+    messages_list.appendChild(ai_msg)
 }
 
 function input_key(e) {
