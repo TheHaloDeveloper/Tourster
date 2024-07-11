07b33770db53fd4939299ac8bde1465adb7dc8ac
Halo <anye.dev.meltdown@gmail.com>
2024-07-11 15:55:45 -0700

    html + python communication!!

--- api/index.py
+++ api/index.py
@@ -1,10 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return render_template('chat.html')
+
+@app.route('/process', methods=['POST'])
+def process():
+    data = request.json
+    message = data.get('message', '')
+    
+    response_message = message[::-1]
+    
+    return jsonify({'response': response_message})
 
 if __name__ == '__main__':
     app.run(debug=True)
