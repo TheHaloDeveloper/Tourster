804657a630295c783806beb3ccd76208516ca930
Halo <anye.dev.meltdown@gmail.com>
2024-07-11 18:52:58 -0700

    basic AI implemented!!!

--- api/index.py
+++ api/index.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template, request, jsonify
+import os
+import google.generativeai as genai
 
 app = Flask(__name__)
 
@@ -6,14 +8,40 @@ app = Flask(__name__)
 def home():
     return render_template('chat.html')
 
+apiKey = ""
+
 @app.route('/ai_response', methods=['POST'])
 def ai_response():
     data = request.json
     message = data.get('message', '')
+
+    # genai.configure(api_key=os.environ["GEMINI_API_KEY"])
+    genai.configure(api_key=apiKey)
+
+    generation_config = {
+        "temperature": 1,
+        "top_p": 0.95,
+        "top_k": 64,
+        "max_output_tokens": 100,
+        "response_mime_type": "text/plain",
+    }
+
+    model = genai.GenerativeModel(
+        model_name="gemini-1.5-flash",
+        generation_config=generation_config,
+    )
+
+    history = [
+        {
+            "role": "model",
+            "parts": ["Hello, I am ToursterAI, created to help you plan your trips. How can I help you today?"]
+        }
+    ]
+
+    chat_session = model.start_chat(history=history)
+    response = chat_session.send_message(message)
     
-    response_message = message[::-1]
-    
-    return jsonify({'response': response_message})
+    return jsonify({'response': response.text})
 
 if __name__ == '__main__':
     app.run(debug=True)
