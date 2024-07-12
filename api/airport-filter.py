b0543ff5b58118d322645187c76f3ff6c0a68d6e
Halo <anye.dev.meltdown@gmail.com>
2024-07-12 14:17:43 -0700

    fixed space in city name bug

--- api/airport-filter.py
+++ api/airport-filter.py
@@ -4,6 +4,9 @@ with open('api/airports.txt') as file:
     for line in file:
         list = re.sub(r'\s+', '.', line.strip()).split('.')
 
+        if len(list) == 5:
+            list[1:3] = [''.join(list[1:3])]
+
         if list[2] == 'USA':
             f = open("api/usa-airports.txt", "a")
             f.write(line)
