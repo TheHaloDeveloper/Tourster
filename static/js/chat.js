63da00c2c1078272e4895adc2fffeaeba292aa23
Halo <anye.dev.meltdown@gmail.com>
2024-07-14 20:52:15 -0700

    separate files

---
+++ static/js/chat.js
@@ -0,0 +1,73 @@
+let message_input = document.getElementById("message_input");
+let messages_list = document.getElementById("messages_list");
+let messages = document.getElementById("messages");
+let mainLogo = document.getElementById("main-logo");
+
+let first_message = false;
+
+function animateLogo() {
+    mainLogo.classList.add("spin-fade");
+}
+
+function msg(sender, text) {
+    let message = document.createElement('li');
+    message.classList.add(`list-${sender}`);
+
+    if(sender == "ai"){
+        message.innerHTML = "";
+        
+        let i = 0;
+        function type() {
+            if (i < text.length) {
+                message.innerHTML += text.charAt(i);
+                i++;
+                setTimeout(type, 30);
+                scrollToBottom();
+            }
+        }
+        type();
+    } else {
+        message.innerHTML = text;
+    }
+
+    messages_list.appendChild(message)
+}
+
+msg("ai", "Hello, I am ToursterAI, created to help you plan your trips. How can I help you today?")
+
+function message_send() {
+    if (message_input.value.replace(/ /g, '') != ''){
+        let req = message_input.value
+        message_input.value = '';
+
+        msg("user",req)
+
+        fetch('/ai_response', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({message: req})
+        }).then(response => response.json()).then(data => {
+            scrollToBottom();
+            msg("ai", data.response)
+
+            if (!first_message) {
+                first_message = true;
+                animateLogo();
+            }
+        })
+    }
+}
+window.message_send = message_send;
+
+function input_key(e) {
+    if (e.keyCode == 13) {
+        message_send();
+    }
+}
+window.input_key = input_key;
+
+function scrollToBottom() {
+    messages.scrollTop = messages.scrollHeight;
+}
---
+++ static/js/home.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const numberOfCircles = 20;
+    const sizes = [40, 20, 20, 60, 20, 50, 150, 25, 15, 30, 90, 30, 50, 80, 90, 40, 70, 35, 45, 65];
+    const positions = [25, 10, 70, 40, 65, 75, 35, 50, 20, 5, 30, 60, 55, 80, 45, 15, 5, 75, 80, 50];
+    const animationDelays = [0, 2, 4, 0, 0, 3, 7, 15, 2, 0, 5, 10, 1, 6, 12, 8, 11, 9, 13, 14];
+    const animationDurations = [15, 12, 25, 18, 25, 25, 25, 25, 15, 11, 20, 12, 17, 19, 30, 27, 14, 16, 23, 21];
+
+    const ul = document.querySelector(".particles");
+    
+    for (let i = 0; i < numberOfCircles; i++) {
+        const li = document.createElement("li");
+        li.style.width = `${sizes[i]}px`;
+        li.style.height = `${sizes[i]}px`;
+        li.style.left = `${positions[i]}%`;
+        li.style.animationDelay = `${animationDelays[i]}s`;
+        li.style.animationDuration = `${animationDurations[i]}s`;
+        ul.appendChild(li);
+    }
+});
