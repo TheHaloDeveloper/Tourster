cc8487eb0b8406d84d9be724bb0d3332ec3bb757
Halo <anye.dev.meltdown@gmail.com>
2024-07-15 23:34:11 -0700

    separate files

--- separator.py
+++ separator.py
@@ -1,21 +1,13 @@
 import ast
 
-types = {}
-categories = {}
+restaurants = open('static/data/restaurants.txt', 'a')
+attractions = open('static/data/attractions.txt', 'a')
 
 with open('static/data/info.txt', 'r') as file: 
     for line in file:
         data = ast.literal_eval(line.strip())
 
-        if data["type"] in types:
-            types[data["type"]] += 1
-        else:
-            types[data["type"]] = 1
-
-        if data["category"] in categories:
-            categories[data["category"]] += 1
-        else:
-            categories[data["category"]] = 1
-
-print(types)
-print(categories)
+        if data["type"] == 'RESTAURANT':
+            restaurants.write(f'{line}')
+        elif data["type"] == 'ATTRACTION':
+            attractions.write(f'{line}')
