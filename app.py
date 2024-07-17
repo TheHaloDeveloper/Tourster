8d6f4d69a49207c7a861c77191952d04e7592ec9
Halo <anye.dev.meltdown@gmail.com>
2024-07-17 15:32:25 -0700

    basic demo with auth!

--- app.py
+++ app.py
@@ -1,17 +1,65 @@
-from flask import Flask, render_template, request, jsonify, send_from_directory
+from flask import Flask, render_template, request, jsonify, send_from_directory, redirect, session, url_for
 from datetime import datetime
 import os
 import google.generativeai as genai
 
-app = Flask(__name__)
+import json
+from os import environ as env
+from urllib.parse import quote_plus, urlencode
 
-@app.route('/')
-def home():
-    return render_template('home.html')
+from authlib.integrations.flask_client import OAuth
+from dotenv import find_dotenv, load_dotenv
+
+load_dotenv()
+
+app = Flask(__name__)
+app.secret_key = env.get("APP_SECRET_KEY")
+
+oauth = OAuth(app)
+oauth.register(
+    "auth0",
+    client_id=env.get("AUTH0_CLIENT_ID"),
+    client_secret=env.get("AUTH0_CLIENT_SECRET"),
+    client_kwargs={
+        "scope": "openid profile email",
+    },
+    server_metadata_url=f'https://{env.get("AUTH0_DOMAIN")}/.well-known/openid-configuration'
+)
 
 @app.route('/soon')
 def serve_soon():
     return send_from_directory('static', 'soon.html')
+
+@app.route("/login")
+def login():
+    return oauth.auth0.authorize_redirect(
+        redirect_uri=url_for("callback", _external=True)
+    )
+
+@app.route("/callback", methods=["GET", "POST"])
+def callback():
+    token = oauth.auth0.authorize_access_token()
+    session["user"] = token
+    return redirect("/")
+
+@app.route("/logout")
+def logout():
+    session.clear()
+    return redirect(
+        "https://" + env.get("AUTH0_DOMAIN")
+        + "/v2/logout?"
+        + urlencode(
+            {
+                "returnTo": url_for("home", _external=True),
+                "client_id": env.get("AUTH0_CLIENT_ID"),
+            },
+            quote_via=quote_plus,
+        )
+    )
+
+@app.route('/')
+def home():
+    return render_template("testhome.html", session=session.get('user'), pretty=json.dumps(session.get('user'), indent=4))
 
 prompt = """
 You are ToursterAI, an AI chatbot who creates full travel plans as an all-in-one tool. 
@@ -82,4 +130,4 @@ def ai_response():
     return jsonify({'response': response.text})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=env.get("PORT", 3000))
