fea09ea7c7aa0ac5f88ef0c64c4273ed25a445dd
Halo <anye.dev.meltdown@gmail.com>
2024-07-18 14:38:49 -0700

    remove

--- flights.py
+++
@@ -1,5 +0,0 @@
-import base64
-
-string = '''CBwQAhoeEgoyMDI0LTA1LTI4agcIARIDVFBFcgcIARIDTVlK    Gh4SCjIwMjQtMDUtMzBqBwgBEgNNWUpyBwgBEgNUUEVAAUgBcAGCAQsI____________AZgBAQ'''
-string += "=" * ((4 - len(string) % 4) % 4)
-print(base64.b64decode(string))
