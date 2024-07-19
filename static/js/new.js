cc9b56d408f500b73674346b95d28460dda4a25a
Halo <anye.dev.meltdown@gmail.com>
2024-07-18 23:46:22 -0700

    both text boxes

--- static/js/new.js
+++ static/js/new.js
@@ -1,36 +1,29 @@
 let array = window.search;
-let input = document.getElementById("from");
+let from = document.getElementById("from");
+let to = document.getElementById("to");
 
-input.addEventListener("keyup", (e) => {
+[from, to].forEach(input => input.addEventListener("keyup", (e) => main(e.target)));
+
+function main(elem) {
     removeElements();
-    let resultCount = 0;
-    for (let i of array) {
-        if (
-            i.toLowerCase().includes(input.value.toLowerCase()) &&
-            input.value != "" &&
-            resultCount < 5
-        ) {
+    array.filter(i => i.toLowerCase().includes(elem.value.toLowerCase()) && elem.value != "")
+         .slice(0, 5)
+         .forEach(i => {
             let listItem = document.createElement("li");
             listItem.classList.add("list-items");
             listItem.style.cursor = "pointer";
-            listItem.setAttribute("onclick", "displayNames('" + i + "')");
-            let index = i.toLowerCase().indexOf(input.value.toLowerCase());
-            let word = i.substr(0, index) + "<b>" + i.substr(index, input.value.length) + "</b>" + i.substr(index + input.value.length);
-            listItem.innerHTML = word;
-            document.querySelector(".list").appendChild(listItem);
-            resultCount++;
-        }
-    }
-});
+            listItem.addEventListener("click", () => displayNames(i, elem));
+            let index = i.toLowerCase().indexOf(elem.value.toLowerCase());
+            listItem.innerHTML = i.substr(0, index) + "<b>" + i.substr(index, elem.value.length) + "</b>" + i.substr(index + elem.value.length);
+            document.querySelector(`#${elem.id}_list`).appendChild(listItem);
+        });
+}
 
-function displayNames(value) {
-    input.value = value;
+function displayNames(value, elem) {
+    elem.value = value;
     removeElements();
 }
 
 function removeElements() {
-    let items = document.querySelectorAll(".list-items");
-    items.forEach((item) => {
-        item.remove();
-    });
+    document.querySelectorAll(".list-items").forEach(item => item.remove());
 }
