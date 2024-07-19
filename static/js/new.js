601de457e238f499ca5e153bb19f803b20dc70e2
Halo <anye.dev.meltdown@gmail.com>
2024-07-18 17:51:32 -0700

    advanced autocomplete!!

--- static/js/new.js
+++ static/js/new.js
@@ -1,6 +1,33 @@
-document.addEventListener('DOMContentLoaded', (event) => {
-    // Access the global variable
-    const array = window.search;
+let array = window.search;
+let input = document.getElementById("from");
 
-    console.log(array)
+input.addEventListener("keyup", (e) => {
+    removeElements();
+    for (let i of array) {
+        if (
+            i.toLowerCase().includes(input.value.toLowerCase()) &&
+            input.value != ""
+        ) {
+            let listItem = document.createElement("li");
+            listItem.classList.add("list-items");
+            listItem.style.cursor = "pointer";
+            listItem.setAttribute("onclick", "displayNames('" + i + "')");
+            let index = i.toLowerCase().indexOf(input.value.toLowerCase());
+            let word = i.substr(0, index) + "<b>" + i.substr(index, input.value.length) + "</b>" + i.substr(index + input.value.length);
+            listItem.innerHTML = word;
+            document.querySelector(".list").appendChild(listItem);
+        }
+    }
 });
+
+function displayNames(value) {
+    input.value = value;
+    removeElements();
+}
+
+function removeElements() {
+    let items = document.querySelectorAll(".list-items");
+    items.forEach((item) => {
+        item.remove();
+    });
+}
