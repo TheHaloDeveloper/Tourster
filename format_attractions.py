b3e0b18055f233690050bee73bd9bcd8e167645d
Halo <anye.dev.meltdown@gmail.com>
2024-07-23 09:42:02 -0700

    removed temp files

--- format_attractions.py
+++
@@ -1,7 +0,0 @@
-f = open("static/data/attractions_formatted.txt", "a")
-
-with open("static/data/attractions.txt", "r") as main:
-    for line in main:
-        f.write(line.replace("'", '"').replace("None", "null"))
-    
-f.close()
