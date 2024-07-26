ae07f10973efc7571ffd64290bf92ee8f53564c5
Halo <anye.dev.meltdown@gmail.com>
2024-07-25 23:43:11 -0700

    remove old auth

--- app.py
+++ app.py
@@ -9,25 +9,12 @@ import json
 from os import environ as env
 from urllib.parse import quote_plus, urlencode
 
-from authlib.integrations.flask_client import OAuth
 from dotenv import find_dotenv, load_dotenv
 import geopy.distance
 
 load_dotenv()
 
 app = Flask(__name__)
-app.secret_key = env.get("APP_SECRET_KEY")
-
-oauth = OAuth(app)
-oauth.register(
-    "auth0",
-    client_id=env.get("AUTH0_CLIENT_ID"),
-    client_secret=env.get("AUTH0_CLIENT_SECRET"),
-    client_kwargs={
-        "scope": "openid profile email",
-    },
-    server_metadata_url=f'https://{env.get("AUTH0_DOMAIN")}/.well-known/openid-configuration'
-)
 
 arr = []
 with open('static/data/search-airports.txt', 'r') as f:
@@ -77,33 +64,6 @@ def serve_privacy():
 @app.route('/cookies')
 def serve_cookies():
     return send_from_directory('static', 'cookies.html')
-
-@app.route("/login")
-def login():
-    return oauth.auth0.authorize_redirect(
-        redirect_uri=url_for("callback", _external=True)
-    )
-
-@app.route("/callback", methods=["GET", "POST"])
-def callback():
-    token = oauth.auth0.authorize_access_token()
-    session["user"] = token
-    return redirect("/")
-
-@app.route("/logout")
-def logout():
-    session.clear()
-    return redirect(
-        "https://" + env.get("AUTH0_DOMAIN")
-        + "/v2/logout?"
-        + urlencode(
-            {
-                "returnTo": url_for("home", _external=True),
-                "client_id": env.get("AUTH0_CLIENT_ID"),
-            },
-            quote_via=quote_plus,
-        )
-    )
 
 @app.route('/')
 def home():
