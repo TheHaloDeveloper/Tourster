84f9b0baf4f8fa04c84c19f281dd1d3e785ec0fd
Halo <anye.dev.meltdown@gmail.com>
2024-07-28 16:04:56 -0700

    hotels name, price, and amenities

--- static/js/trip.js
+++ static/js/trip.js
@@ -207,6 +207,81 @@ function createItenerary(){
     }
 }
 
+let amenities = {
+    "wifi": "wifi",
+    "parking": "square-parking",
+    "air conditioning": "fan",
+    "restaurant": "utensils",
+    "accessible": "wheelchair",
+    "smoke-free": "ban-smoking",
+    "pool": "person-swimming",
+    "pet-friendly": "paw",
+    "gym": "dumbbell",
+    "spa": "spa",
+    "bar": "wine-glass",
+    "room service": "user-shield",
+    "laundry": "jug-detergent",
+    "airport shuttle": "van-shuttle",
+    "golf": "golf-ball-tee",
+    "beach access": "umbrella-beach"
+};
+
+function addHotel(hotel) {
+    let a = hotel.amenities;
+    let count = 0;
+    let table = `<table class="h-amenities">`
+    
+    if (a) {
+        for (let i = 0; i < a.length; i++) {
+            let icon;
+            let t = a[i].trim().toLowerCase();
+
+            if (t.includes('wi-fi')) {
+                icon = amenities['wifi'];
+            }
+            if (t.includes('parking')) {
+                icon = amenities['parking'];
+            }
+            if(t.includes('smoke-free')) {
+                icon = amenities['smoke-free'];
+            }
+            if(t.includes('pool')) {
+                icon = amenities['pool'];
+            }
+            if (!icon) {
+                icon = amenities[t];
+            }
+
+            if (count % 3 == 0) {
+                if (count != 0){
+                    table += `</tr>`;
+                }
+                table += `<tr>`;
+            }
+
+            table += `<td><i class="fa-solid fa-${icon}"></i> ${a[i]}</td>`
+
+            count += 1;
+        }
+
+        if (count % 3 != 0) {
+            table += `</tr>`;
+        }
+        table += `</table>`;
+
+        console.log(hotel)
+
+        document.getElementById('hotels-container').innerHTML += `
+            <div class="hotel">
+                <img class="h-image" src="${hotel.images[0]}"></img>
+                <div class="h-name">${hotel.name}</div>
+                <div class="h-price">${hotel.lowest}</div>
+                ${table}
+            </div>
+            `;
+    }
+}
+
 let map;
 let mapLoaded = false;
 let remainingHotels = [];
@@ -230,8 +305,11 @@ function allocationComplete(){
         }
     }
 
-    remainingHotels.sort((a, b) => a[0] - b[0]).reverse()
-    console.log(remainingHotels.splice(0, 6))
+    remainingHotels.sort((a, b) => a[0] - b[0]).reverse();
+
+    for(let hotel of remainingHotels.splice(0, 6)) {
+        addHotel(hotel[1])
+    }
 
     //Atractions
     for (let i = data['attractions'].length - 1; i >= 0; i--) {
